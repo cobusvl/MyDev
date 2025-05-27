401b279eb5c34b0cb5ee24cfb5c753b2a5f83cb3
Cobus <135943427+cobusvl@users.noreply.github.com>
2025-05-27 06:55:42 +0200

    Add files via upload

---
+++ lang/en/quizaccess_redirectonpass.php
@@ -0,0 +1,17 @@
+<?php
+// This file defines all strings for the quizaccess_redirectonpass plugin in English.
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Redirect on Pass';
+$string['accessrule'] = 'Redirect on Pass'; // This is crucial for the heading in settings.
+$string['redirectonpass_header'] = 'Redirect on Pass Settings'; // New header string
+$string['redirectonpass_enabled'] = 'Enable redirect on pass';
+$string['redirectonpass_target'] = 'Redirect to activity';
+$string['enableredirect_help'] = 'If enabled, the user will be redirected to a selected activity upon passing the quiz.';
+
+$string['redirectactivity'] = 'Redirect to activity';
+$string['redirectactivity_help'] = 'Select the activity within this course where the user should be redirected if they pass the quiz. Select "Do not redirect" to disable.';
+
+$string['donotredirect'] = 'Do not redirect'; // Option in the dropdown.
+$string['redirecting'] = 'Congratulations! Redirecting you now...'; // Message shown during redirection.
